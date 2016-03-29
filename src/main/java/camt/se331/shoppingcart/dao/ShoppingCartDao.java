dfa292d14edfcd20d7b6097312dd15eab586d85e
AgentPleum <pajera_kasiprong@cmu.ac.th>
2016-03-29 22:20:56 +0700

    Q8 v.1

--- src/main/java/camt/se331/shoppingcart/dao/ShoppingCartDao.java
+++ src/main/java/camt/se331/shoppingcart/dao/ShoppingCartDao.java
@@ -1,17 +1,25 @@
 package camt.se331.shoppingcart.dao;
 
+import camt.se331.shoppingcart.entity.SelectedProduct;
 import camt.se331.shoppingcart.entity.ShoppingCart;
+import org.hibernate.annotations.*;
+import org.hibernate.annotations.CascadeType;
 
+import javax.persistence.*;
+import javax.persistence.Entity;
 import java.util.Date;
 import java.util.List;
 
 /**
  * Created by Dto on 2/7/2015.
  */
+
 public interface ShoppingCartDao {
+
     List<ShoppingCart> getShoppingCarts();
     List<ShoppingCart> getShoppingCartBefore(Date stateDate);
     List<ShoppingCart> getShoppingCartBetween(Date stateDate, Date stopDate);
     ShoppingCart addShoppingCart(ShoppingCart shoppingCart);
     ShoppingCart deleteShoppingCart(ShoppingCart shoppingCart);
+    ShoppingCart findById(Long id);
 }
--- src/main/java/camt/se331/shoppingcart/entity/ShoppingCart.java
+++ src/main/java/camt/se331/shoppingcart/entity/ShoppingCart.java
@@ -1,13 +1,27 @@
 package camt.se331.shoppingcart.entity;
 
+import org.hibernate.annotations.*;
+
+import javax.persistence.*;
+import javax.persistence.Entity;
 import java.util.Date;
 import java.util.List;
 
 /**
  * Created by Dto on 2/7/2015.
  */
+
+@Entity
 public class ShoppingCart {
+    @Id
+    @GeneratedValue
+    Long id;
+
+    @OneToMany(fetch = FetchType.EAGER)
+    @Cascade(org.hibernate.annotations.CascadeType.ALL)
+
     List<SelectedProduct> selectedProducts;
+    @Temporal(TemporalType.TIMESTAMP)
     Date purchaseDate;
     public double getTotalProductPrice(){
         return 0.0;
