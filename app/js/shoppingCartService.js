c7be747760337f3d80734d0c142c7335b03b4107
AgentPleum <pajera_kasiprong@cmu.ac.th>
2016-03-30 08:05:10 +0700

    Edit

---
+++ app/js/shoppingCartService.js
@@ -0,0 +1,13 @@
+/**
+ * Created by HH on 2016/3/26.
+ */
+'use strict'
+
+var shoppingCartServices = angular.module('shoppingCartServices',['ngResource']);
+shoppingCartServices.factory('shoppingCartService',['$resource','API_URL',function($resource,API_URL){
+    return $resource(API_URL+'/shoppingcart/:id', { id: '@_id' }, {
+        update: {
+            method: 'PUT' // this method issues a PUT request
+        }});
+
+}])
